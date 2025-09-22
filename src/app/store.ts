663da68fce0bc6a6897bd47e51f28a647f06b71f
Eling Meyatmaja <eling.atma@gmail.com>
2025-09-22 21:12:21 +0700

    19 Update layout

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
-import {
-    configureStore,
-} from '@reduxjs/toolkit'
-import { loginSlice } from './login/redux/login_slice';
-const store = configureStore({
-    reducer: {
-        // Add the generated reducer as a specific top-level slice
-        login: loginSlice.reducer,
-    },
-})
-export default store;
+// app/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import { loginSlice } from "./login/redux/login_slice";
+
+export const store = configureStore({
+  reducer: {
+    login: loginSlice.reducer,
+  },
+});
+
+// Type helpers
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
